028f2f0706c7e7e38f6c6ef32b7f2cafafe67f44
anubhav-gupta-software <anubhavgupta.main@gmail.com>
2024-06-10 11:16:53 +0530

    variables.

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,24 @@
 console.log('hello bro');
+let x = 5;
+x++;
+console.log(x);
+// alert("wassup gg")
+x = null; 
+y = undefined; 
+console.log(x)
+console.log(y)
+x = true;
+// always use camelCase because beautiful
+
+// let, const, var
+
+// let - variable can't be re-declared but can be updated. block scope.
+let namea = "tony"
+console.log(namea);
+// let namea  = 34; redeclaring not possible
+
+//var - variables can be re-declared and updated. global scope.
+var joe = 1;
+var joe = 34;
+
+//const - variable can't be re-declared or updated. block scope.
