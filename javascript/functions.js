272c9f4ce4c2eb06a04150b876f9a64d16af13c5
anubhav-gupta-software <anubhavgupta.main@gmail.com>
2024-06-12 18:46:42 +0530

    js_basics_complete

--- javascript/functions.js
+++ javascript/functions.js
@@ -33,4 +33,47 @@ let age = [1,2,3,4,5,6,7];
 age.forEach((val)=>{
     console.log(val);
 });
+// in for each callback function, we can use 3 parameters, value, index, array
 
+age.forEach((val,idx,ar)=>
+{
+    console.log(val, idx, ar);
+});
+
+
+
+//Some important array methods
+
+//map method, returns a new array, creates a new one
+//the value its callback returns are used to form new array
+//the old array remains unchanged
+let y = age.map((val)=> {
+    console.log(val);
+    return val*5;
+
+});
+
+console.log(y);
+6
+
+//filter
+//creates a new array of elements that give true for a condition;
+let newAge = age.filter((val)=>{
+    return val%2==0;
+});
+
+console.log(age);
+console.log(newAge);
+
+//reduce method
+//performs operations to reduce the array to a single value
+
+let totalAge = age.reduce((res, curr)=>{
+    return res+curr;
+});
+
+console.log(totalAge);
+//
+
+// let userInput = prompt("Give me your number")
+// console.log(userInput); for browser
