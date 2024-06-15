801685efaa4ec1c3566a2a27cfabdc7e280716fa
anubhav-gupta-software <anubhavgupta.main@gmail.com>
2024-06-15 12:09:38 +0530

    DOM BASICS OVER

---
+++ DOM and events/dompt2.js
@@ -0,0 +1,13 @@
+//ATTRIBUTES
+// getAttribute(attr) - to get the attribute value
+// setAttribute(attr, value) - to set the attribute value
+
+let div = document.querySelector("#box");
+console.log(div);
+// let id = div.getAttribute("id");
+// console.log(id);
+//document.querySelector("#box").setAttribute("id","new_box");
+
+//STYLING
+div.style.backgroundColor="green";
+console.log(div.style.backgroundColor);
